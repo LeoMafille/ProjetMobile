7f72cbdd40d5b9989dbb8fbcf0802e7034f6577f
Jean-Victor Lecroisey <lecrois211@campus.unicaen.fr>
2023-03-20 13:46:17 +0100

    ajout menu déroulant

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,22 +21,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ModalDrawer(drawerContent = {Menu()}) {
+                        rememberDrawerState(initialValue = DrawerValue.Closed)
+                    }
+                        Greeting(modifier = Modifier)
+                    }
                 }
             }
         }
     }
-}
 
+//menu déroulant
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Menu(){
+    Column() {
+        Text(text = "Menu : ")
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Liste des formations")
+        }
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Liste des élèves")
+        }
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Liste des scéances à venir")
+        }
+        TextButton(onClick = { /*TODO*/ }) {
+            Text(text = "Evaluations")
+        }
+    }
+}
+//greeting avec placholder
+@Composable
+fun Greeting(modifier: Modifier) {
+    Text(text = "Hola chicos :)")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ApplicationTheme {
-        Greeting("Android")
+        Greeting(Modifier)
     }
 }
