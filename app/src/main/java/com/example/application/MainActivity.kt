7a079276b98c313d9e848a5c3f460fdf97c46585
louise coueffin <21903623@etu.unicaen.fr>
2023-03-20 10:17:03 +0100

    :)

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.appsae
+package com.example.application
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.appsae.ui.theme.AppSaeTheme
+import com.example.application.ui.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppSaeTheme {
+            ApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AppSaeTheme {
+    ApplicationTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/application/ui/theme/Color.kt
+++ app/src/main/java/com/example/application/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.appsae.ui.theme
+package com.example.application.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
