90d1c39a475dde426039213a689f5d7f3092d670
Jean-Victor Lecroisey <lecrois211@campus.unicaen.fr>
2023-03-20 14:38:38 +0100

    ajout menu dessus de lappli

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.application.ui.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +27,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    //ajout du menu déroulant a l'activité
                     ModalDrawer(drawerContent = {Menu()}) {
                         rememberDrawerState(initialValue = DrawerValue.Closed)
                     }
-                        Greeting(modifier = Modifier)
+                        MainCompose(modifier = Modifier)
                     }
                 }
             }
@@ -34,7 +41,10 @@ class MainActivity : ComponentActivity() {
 //menu déroulant
 @Composable
 fun Menu(){
+    Row() {
     Column() {
+        Text(text = "")
+        Text(text = "")
         Text(text = "Menu : ")
         TextButton(onClick = { /*TODO*/ }) {
             Text(text = "Liste des formations")
@@ -49,17 +59,41 @@ fun Menu(){
             Text(text = "Evaluations")
         }
     }
+    }
 }
-//greeting avec placholder
+//greeting avec placeholder
 @Composable
-fun Greeting(modifier: Modifier) {
-    Text(text = "Hola chicos :)")
+fun MainCompose(modifier: Modifier) {
+    var etat = remember {
+        mutableStateOf(0)
+    }
+    Column() {
+        Row() {
+            OutlinedButton(onClick = { etat.value = 1 }, Modifier.background(if (etat.value==1) Color.Red else Color.Black))  {
+                Text(text = "Liste des élèves")
+            }
+            OutlinedButton(onClick = { etat.value = 2 }, modifier = Modifier.background(if (etat.value==2) Color.Red else Color.Black) ) {
+                Text(text = "Liste des formations")
+            }
+            IconButton(onClick = { etat.value = 3}, Modifier.background(if (etat.value==3) Color.Red else Color.Black)) {
+                Icon(painter = painterResource(id = R.drawable.profil), contentDescription = "icone profil")
+            }
+        }
+//menu dessus appli
+        Row(modifier) {
+            when (etat.value) {
+                1 -> Text(text = "Liste des élèves :", modifier.fillMaxSize(), textAlign = TextAlign.Center)
+                2 -> Text(text = "Liste des formations :", modifier.fillMaxSize(), textAlign = TextAlign.Center)
+                3 -> Text(text = "Profil :", modifier.fillMaxSize(), textAlign = TextAlign.Center)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ApplicationTheme {
-        Greeting(Modifier)
+        MainCompose(Modifier)
     }
 }
